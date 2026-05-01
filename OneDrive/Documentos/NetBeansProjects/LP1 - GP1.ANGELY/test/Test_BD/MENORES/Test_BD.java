a1a5a77a70f426f67b65a455a5a9e3464e975a54
Frocmen <Frocmen@LAPTOP-DLNMD6KR>
2026-05-01 01:05:01 -0500

    PRIMERA MODIFICACIÓN - Añadimos nuevas carpetas

---
+++ OneDrive/Documentos/NetBeansProjects/LP1 - GP1.ANGELY/test/Test_BD/MENORES/Test_BD.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Test_BD.MENORES;
+
+import Util.ConexionSingleton;
+import java.sql.*;
+
+
+public class Test_BD {
+
+    
+    public static void main(String[] args) {
+      Test_BD t = new Test_BD();
+      t.testConexion();
+    }
+    
+    public void testConexion(){
+        ConexionSingleton conn = new ConexionSingleton();
+        try {
+            Connection connection = conn.getConnection();
+            if (connection != null && !connection.isClosed()) {
+                System.out.println("Conexion Satisfactoria!!!!");
+            }else{
+                System.out.println(" |ERROR| - CONEXIÓN FALLIDA");
+            }
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+            e.printStackTrace();
+            
+        }
+    }
+    
+}
